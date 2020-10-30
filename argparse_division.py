79489e51970c9729116ca9d94d3329b99d568472
Madi <72963376+Madi-S@users.noreply.github.com>
2020-10-30 20:09:11 +0600

    Update argparse_division.py

--- argparse_division.py
+++ argparse_division.py
@@ -1,5 +1,5 @@
 # Here we learn about:
-# 1) Positional (mandatory) arguments, which do no include '-'
+# 1) Positional (mandatory) arguments, which do not include '-'
 # 2) Some advanced parameters for creating an argument
 
 
@@ -13,15 +13,15 @@ help_info_display = 'Pass two numbers for division'
 parser.add_argument(argument_display_name,
                     help=help_info_display, type=int, nargs=2)
                     
-# nargs - takes '+' for 1 or more arguments, or specified number of arguments needed (int)
-# type - (str be default) specify the type of argument  needed
+# nargs - takes '+' for 1 or more arguments or a specified number of arguments needed (int)
+# type - (str be the default) specify the type of argument  needed
 # help - information that displays when --help or -h is called
 # first two parameters - short and long names for parameters (like div and division), but positional arguments take only one name without '-'
 
 args = parser.parse_args()
 
 if args.nums:
-    # args.nums is a list, so we can acess numbers by indexes
+    # args.nums is a list, so we can access numbers by indexes
     print(args.nums[0] // args.nums[1])
     
     
