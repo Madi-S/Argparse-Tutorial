c00fe52a2525d3212d2441fc80116782b2fa57c0
Madi <72963376+Madi-S@users.noreply.github.com>
2020-10-30 20:02:13 +0600

    Update argparse_division.py

--- argparse_division.py
+++ argparse_division.py
@@ -23,3 +23,31 @@ args = parser.parse_args()
 if args.nums:
     # args.nums is a list, so we can acess numbers by indexes
     print(args.nums[0] // args.nums[1])
+    
+    
+    
+'''
+Example of usage:
+
+1.
+λ python argparse_division.py 5 2
+2
+
+
+2.
+λ python argparse_division.py 5 2 3
+usage: argparse_division.py [-h] nums nums
+argparse_division.py: error: unrecognized arguments: 3
+
+
+3.
+λ python argparse_division.py
+usage: argparse_division.py [-h] nums nums
+argparse_division.py: error: the following arguments are required: nums
+
+
+4.
+λ python argparse_division.py one two
+usage: argparse_division.py [-h] nums nums
+argparse_division.py: error: argument nums: invalid int value: 'one'
+'''
