fd770748ff7cbf78e120a422b94b623475cc03f0
Madi <72963376+Madi-S@users.noreply.github.com>
2020-10-30 19:52:52 +0600

    Update argparse_avanced.py

--- argparse_avanced.py
+++ argparse_avanced.py
@@ -1,11 +1,13 @@
+# Here we learn about:
+# 1) Combining positional and optional arguments
+# 2) How to set the default value for the argument
+# 3) Make user choose the value for the argument from the given list
+
 import argparse
 import math
 from time import sleep
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
-
-# First two examples cover 90% of needs, however, sometimes some advanced techincs must be used:
-#
 
 
 parser = argparse.ArgumentParser(description='Argparse tutorial')
@@ -14,10 +16,14 @@ parser = argparse.ArgumentParser(description='Argparse tutorial')
 parser.add_argument(
     '-m', '--math', help='Show mathematical constants', choices=['e', 'pi'])
 
+# Choices takes list with values that can be specified
+# If user specifies the value that does not exists in the choices list, the program will throw an error
+
 # When argument can be modified (set default for non-given arguments):
 parser.add_argument('-e', '--exc_path', help='Specify executable path for the ChromeDriver',
                     default=r'C:\Users\khova\Desktop\Python\chromedriver.exe')
 
+# Specify the default value in default that can be modified or left like that
 
 args = parser.parse_args()
 
